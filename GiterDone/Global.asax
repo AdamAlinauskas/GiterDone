cfbed0d41a7a8173c198bc76382fc15f5bf68226
Adam Alinauskas <aalinauskas@gmail.com>
2011-07-24 19:39:35 -0600

    Starting to implement the task index view with knockout js.

---
+++ GiterDone/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GiterDone.MvcApplication" Language="C#" %>
---
+++ GiterDone/Scripts/MyScripts/Tasks.js
@@ -0,0 +1,18 @@
+﻿$(function () {
+    function task(name, isDone) {
+        this.name = ko.observable(name);
+        this.isDone = isDone;
+    }
+
+    var tasksViewModel = {
+        tasks: ko.observableArray([]),
+
+        newTaskText: ko.observable(".....")
+    };
+
+    tasksViewModel.tasks().push(new task("blah"));
+
+    ko.applyBindings(tasksViewModel);
+
+
+});
