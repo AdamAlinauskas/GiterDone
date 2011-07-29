63b3c6980e8ecd17e27dc22f8e03d89be3b15bf2
Adam Alinauskas <aalinauskas@gmail.com>
2011-07-29 15:28:29 -0600

    Hitting the enter button works when adding a task. Added a data-bind event to the form.

--- GiterDone/Scripts/MyScripts/Tasks.js
+++ GiterDone/Scripts/MyScripts/Tasks.js
@@ -7,11 +7,14 @@
     var tasksViewModel = {
         tasks: ko.observableArray([]),
         addTask: function () { this.tasks.push(new task(this.newTaskText())); },
-        newTaskText: ko.observable("")
+        newTaskText: ko.observable(""),
+        //save: function () { window.localStorage.setItem("tasks", JSON.stringify(ko.toJSON())); }
     };
+
+    //var test = window.localStorage.getItem("tasks");
     
     tasksViewModel.tasks.push(new task("Drive to a farm.", true));
     tasksViewModel.tasks.push(new task("Tip a cow.", true));
-    
+
     ko.applyBindings(tasksViewModel);
 });
