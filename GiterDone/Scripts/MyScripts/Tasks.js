5faf2f44428f6ac6acad6fa6ea29dc3e853bb3a5
Adam Alinauskas <aalinauskas@gmail.com>
2011-07-29 13:58:43 -0600

    Added isDone to task list.

--- GiterDone/Scripts/MyScripts/Tasks.js
+++ GiterDone/Scripts/MyScripts/Tasks.js
@@ -7,10 +7,11 @@
     var tasksViewModel = {
         tasks: ko.observableArray([]),
         addTask: function () { this.tasks.push(new task(this.newTaskText())); },
-        newTaskText: ko.observable(".....")
+        newTaskText: ko.observable("")
     };
-
-    tasksViewModel.tasks().push(new task("Get knockout working with templates"));
-
+    
+    tasksViewModel.tasks.push(new task("Drive to a farm.", true));
+    tasksViewModel.tasks.push(new task("Tip a cow.", true));
+    
     ko.applyBindings(tasksViewModel);
 });
